68e67824b05a2014c2330245cf82cb2112b9aa85
aailyin <psamusev@github.com>
2014-02-28 14:23:01 +0300

    Change folder structure. Added first pages and server routes

---
+++ Application/app.js
@@ -0,0 +1,27 @@
+/**
+ * Created by AA on 26.02.14.
+ */
+var express = require('express'),
+    http = require('http'),
+    fs = require('fs'),
+    url = require('url'),
+    util = require('util');
+
+var app = express();
+
+app.configure(function (){
+    app.set('title', 'Timetable BSU');
+    app.use(express.logger());
+    app.use(express.static(__dirname));
+});
+
+app.get('/', function (req, res){
+    res.sendfile(__dirname + '/Frontend/index.html');
+});
+
+app.get('*', function (req, res){
+    res.sendfile(__dirname + '/Frontend/404.html');
+});
+
+app.listen(8000);
+
